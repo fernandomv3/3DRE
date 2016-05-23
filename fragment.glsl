a9429679269127a4b855bea53370cd35c8a55d0d
fernandomv3 <fernandomv3@gmail.com>
2016-05-22 19:12:41 -0500

    vertex and fragment shaders modified to use texture

--- fragment.glsl
+++ fragment.glsl
@@ -1,11 +1,13 @@
 #version 330
 
 in vec4 fNormal;
+in vec2 fUv;
 layout(location = 0) out vec4 color;
 uniform float gamma;
 uniform float time;
 uniform int screenWidth;
 uniform int screenHeight;
+uniform sampler2D colorMap;
 
 struct Material{
 	vec4 diffuse;
@@ -16,10 +18,6 @@ struct Material{
 uniform Material material;
 
 void main(){
-  vec3 col = vec3(sin(time),cos(time),0.0);
-  //col = pow(col,vec3(1/gamma));
-  col = (col*0.5) + 0.5;
-  //color = pow((normalize(fNormal)*0.5)+0.5,vec4(gamma,gamma,gamma,1.0));
-  color = vec4(gl_FragCoord.x/screenWidth,gl_FragCoord.y/screenHeight,0.0,0.0);
-  color = material.diffuse;
+  vec4 diffuseColor = texture(colorMap,fUv);
+  color = diffuseColor;
 }
--- vertex.glsl
+++ vertex.glsl
@@ -7,10 +7,13 @@ uniform mat4 normalModelMatrix;
 
 layout(location = 0) in vec3 vPosition;
 layout(location = 1) in vec3 vNormal;
+layout(location = 2) in vec2 vUv;
 
 out vec4 fNormal;
+out vec2 fUv;
 
 void main(){
   fNormal = worldMatrix * normalModelMatrix * vec4(vNormal,0.0);
+  fUv = vUv;
   gl_Position =  projectionMatrix * worldMatrix * modelMatrix *vec4(vPosition,1.0);
 }
