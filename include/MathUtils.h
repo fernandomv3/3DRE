141583441269daa139fce24d0f0e7b02e979d8a2
fernandomv3 <fernandomv3@gmail.com>
2016-06-17 09:01:47 -0500

    added random float generator helper function, uuid generation now uses this new random

--- include/MathUtils.h
+++ include/MathUtils.h
@@ -5,5 +5,11 @@
 
 //Adapted from https://gist.github.com/ne-sachirou/882192
 std::string generateUUID();
+
+//clamps from -1 to 1
 float clamp(float value);
+
+float lerp(float a, float b, float w);
+
+float random(float min,float max);
 #endif
--- src/MathUtils.cpp
+++ src/MathUtils.cpp
@@ -18,7 +18,7 @@ std::string generateUUID(){
   for(int i=0;i<36;i++){
     if (i != 8 && i != 13 && i != 18 && i != 14 && i != 23) {
       if (rnd <= 0x02) {
-          rnd = (0x2000000 + (std::rand() * 0x1000000)) | 0;
+          rnd = (0x2000000 + static_cast <int> (random(0.0,1.0) * 0x1000000)) | 0;
       }
       rnd >>= 4;
       r = rnd & 0xf;
@@ -30,4 +30,12 @@ std::string generateUUID(){
 
 float clamp(float value){
   return fmin(fmax(value,-1.0f),1.0f);
+}
+
+float lerp(float a, float b, float w){
+  return a + w*(b-a);
+}
+
+float random(float min, float max){
+  return min + static_cast <float> (std::rand()) /( static_cast <float> (RAND_MAX/(max-min)));
 }
