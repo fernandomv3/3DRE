bcf1cccf76f78ed7a2d80586855430f8382876ee
fernandomv3 <fernandomv3@gmail.com>
2016-06-28 16:12:21 -0500

    new scene with 25 spheres and functions for generating ssao kernel and random texture

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,80 @@
 #include "Material.h"
 #include "Mesh.h"
 #include "Renderer.h"
+#include "MathUtils.h"
 
-bool handleEvents(){
+bool handleEvents(Camera& cam, Scene& scene, Light& light){
   SDL_Event event;
   while(SDL_PollEvent(&event)){
     switch(event.type){
       case SDL_QUIT:
         return true;
+      case SDL_KEYDOWN:
+        switch(event.key.keysym.sym){
+          case SDLK_w:
+            cam.getPosition()[1] += 0.1;
+            break;
+          case SDLK_s:
+            cam.getPosition()[1] -=0.1;
+            break;
+          case SDLK_a:
+            cam.getPosition()[0] += 0.1;
+            break;
+          case SDLK_d:
+            cam.getPosition()[0] -= 0.1;
+            break;
+          case SDLK_l:
+            light.getPosition()[0] += 0.1;
+            break;
+          case SDLK_j:
+            light.getPosition()[0] -= 0.1;
+            break;
+          case SDLK_i:
+            light.getPosition()[1] += 0.1;
+            break;
+          case SDLK_k:
+            light.getPosition()[1] -= 0.1;
+            break;
+          case SDLK_u:
+            light.getPosition()[2] -= 0.1;
+            break;
+          case SDLK_o:
+            light.getPosition()[2] += 0.1;
+            break;
+      }
     }
   }
   return false;
+}
+
+std::vector<Vec4> generateSsaoKernel(int kernelSize){
+  std::vector<Vec4> kernel;
+  for(int i= 0; i < kernelSize;i++){
+    Vec4 v = Vec4(random(-1.0,1.0),random(-1.0,1.0),random(0.0,1.0),0.0).normalize();
+    float scale = float(i)/float(kernelSize);
+    scale = lerp(0.1,1.0,scale*scale);
+    v *= scale;
+    kernel.push_back(v);
+  }
+  return kernel;
+}
+
+std::shared_ptr<Texture> generateSsaoNoise(int noiseSize){
+  auto tex = std::make_shared<Texture>(noiseSize,noiseSize);
+  tex->setType(std::type_index(typeid(float)));
+  tex->setInnerFormat("RGBA16F");
+  tex->setWrapping(std::make_tuple("repeat","repeat",""));
+  std::vector<float> noise;
+  for(int i = 0,size=noiseSize*noiseSize;i< size;i++){
+    Vec4 v = Vec4(random(-1.0,1.0),random(-1.0,1.0),0.0,0.0).normalize();
+    noise.push_back(v[0]);
+    noise.push_back(v[1]);
+    noise.push_back(v[2]);
+    noise.push_back(v[3]);
+  }
+  tex->setType(std::type_index(typeid(float)));
+  tex->loadData(noise.data(),noiseSize,noiseSize);
+  return tex;
 }
 
 int main(int argc, char** argv){
@@ -32,6 +96,7 @@ int main(int argc, char** argv){
   Camera cam, lightCam;
   Scene scene, scene2;
   Renderer renderer(SCREEN_WIDTH,SCREEN_HEIGHT);
+
   //create shadowmap framebuffer
   auto shadowFb = std::make_shared<Framebuffer>(Framebuffer(SCREEN_WIDTH,SCREEN_HEIGHT));
   auto depthMap = std::make_shared<Texture>(Texture(SCREEN_WIDTH,SCREEN_HEIGHT));
@@ -40,9 +105,8 @@ int main(int argc, char** argv){
   shadowFb->addRenderTarget("depthMap",depthMap);
   auto depthMaterial = std::make_shared<Material>(Material());
   depthMaterial->setShaderFiles("/home/fernando/Projects/engine/shaders/vertex-transform.glsl","/home/fernando/Projects/engine/shaders/fragment-shadow.glsl");
-
+  
   //create gBuffer
-
   auto gBuffer = std::make_shared<Framebuffer>(Framebuffer(SCREEN_WIDTH,SCREEN_HEIGHT));
   auto positionTarget = std::make_shared<Texture>(Texture(SCREEN_WIDTH,SCREEN_HEIGHT));
   positionTarget->setType(std::type_index(typeid(float)));
@@ -64,28 +128,11 @@ int main(int argc, char** argv){
   //init cameras
   cam.perspectiveCamera(30.0,float(SCREEN_WIDTH)/float(SCREEN_HEIGHT),0.1,100.0);
   cam.getPosition()[2] = 10;
+  cam.updateWorldMatrix();
+  cam.setTarget(std::make_shared<Vec4>(Vec4(0.0,0.0,0.0,1.0)));
+
   lightCam.perspectiveCamera(30.0,float(SCREEN_WIDTH)/float(SCREEN_HEIGHT),0.1,100.0);
   lightCam.setTarget(std::make_shared<Vec4>(Vec4(0.0,0.0,0.0,1.0)));
-
-  //add robot
-  auto geometry = std::make_shared<Geometry>(loadDataFromFile("/home/fernando/Projects/engine/models/robot-low-normal.dae"));
-  auto phongMat = std::make_shared<Material>(Material(Vec4(0.5,0.5,0.5,0.0),Vec4(1.0,1.0,1.0,0.0),20));
-  phongMat->setShaderFiles("/home/fernando/Projects/engine/shaders/vertex_deferred.glsl","/home/fernando/Projects/engine/shaders/fragment_robot_deferred.glsl");
-  auto robot = std::make_shared<Mesh>(Mesh(geometry,phongMat));
-
-  auto texture = std::make_shared<Texture>(Texture("/home/fernando/Projects/engine/textures/normalMap.jpg"));
-  texture->setGamma(false);
-  texture->setNMipmaps(0);
-  texture->setFiltering(std::make_pair("nearest","nearest"));
-  phongMat->setNormalMap(texture);
-  auto specTexture = std::make_shared<Texture>(Texture("/home/fernando/Projects/engine/textures/perlin.png"));
-  specTexture->setGamma(false);
-  specTexture->setNMipmaps(0);
-  specTexture->setFiltering(std::make_pair("nearest","nearest"));
-  phongMat->setSpecularMap(specTexture);
-  scene.add(robot);
-  robot->getRotation()[0] = 90;
-  robot->getRotation()[2] = 0;
 
   auto quad = std::make_shared<Geometry>(quadGeometry());//shared geometry
 
@@ -104,12 +151,27 @@ int main(int argc, char** argv){
   floorMat->setNormalMap(floorNormal);
   floorMat->setColorMap(tex);
   floorQuad->setScale(Vec4(10,10,10,1));
-  
   floorQuad->getPosition()[1] = -2.3;
   floorQuad->getRotation()[0] = 90;
   floorQuad->getPosition()[2] = -5;
   scene.add(floorQuad);
 
+  //add spheres
+  auto geometry = std::make_shared<Geometry>(loadDataFromFile("/home/fernando/Projects/engine/models/icosphere.dae"));
+  auto phongMat = std::make_shared<Material>(Material(Vec4(0.5,0.5,1.0,0.0),Vec4(0.0,0.0,0.0,0.0),20));
+  phongMat->setShaderFiles("/home/fernando/Projects/engine/shaders/vertex_deferred.glsl","/home/fernando/Projects/engine/shaders/fragment_sphere_deferred.glsl");
+  std::vector<std::shared_ptr<Mesh>> sphereArray;
+  for(int i = 0; i < 5;i++){
+    for(int j = 0; j < 5 ; j++){
+      auto mesh = std::make_shared<Mesh>(Mesh(geometry,phongMat));
+      mesh->setPosition(Vec4(i-2,-1.8,j,1.0));
+      mesh->setScale(Vec4(0.5,0.5,0.5,1.0));
+      sphereArray.push_back(mesh);
+    }
+  }
+  for(auto m : sphereArray){
+    scene.add(m);
+  }
   //add directional light
   auto dirlight = std::make_shared<Light>(Light(Vec4(0.8,0.8,0.8,1.0)));
   dirlight->setMaterial(quadMat);
@@ -118,38 +180,40 @@ int main(int argc, char** argv){
   quadMat->setDepthMap(depthMap);
   scene2.add(dirlight);
 
+  scene2.setSsaoKernel(generateSsaoKernel(8));
+  scene2.setSsaoNoise(generateSsaoNoise(4));
+  scene2.setSsaoRadius(1);
+
   bool quit = false;
   float currTime = SDL_GetTicks();
-  glClearColor(1.0,1.0,1.0,1.0);
+  glClearColor(0.0,0.0,0.0,0.0);
 
   while(!quit){
     float newTime = SDL_GetTicks();
     float dt = (newTime - currTime)*0.001;
     currTime = newTime;
 
-    quit = handleEvents();
+    quit = handleEvents(cam,scene,*dirlight);
     float t = currTime*0.001;
     renderer.setTime(t);
-    robot->getRotation()[2] -= 50*dt;
 
     lightCam.setPosition(dirlight->getPosition());
-
-    robot->setMaterial(depthMaterial);
+    for(auto m : sphereArray) m->setMaterial(depthMaterial);
     floorQuad->setMaterial(depthMaterial);
     renderer.setReadFramebuffer(nullptr);
     renderer.setWriteFramebuffer(shadowFb);
-    //glCullFace(GL_FRONT);
+    glCullFace(GL_FRONT);
     renderer.render(scene,lightCam,"shadow");
-    //glCullFace(GL_BACK);
+    glCullFace(GL_BACK);
     dirlight->setLightMatrix(cross(lightCam.getProjectionMatrix(),lightCam.getWorldMatrix()));
 
-    robot->setMaterial(phongMat);
+    for(auto m : sphereArray) m->setMaterial(phongMat);
     floorQuad->setMaterial(floorMat);
 
     renderer.setReadFramebuffer(nullptr);
     renderer.setWriteFramebuffer(gBuffer);
     renderer.render(scene,cam,"geometry");
-    
+
     renderer.setReadFramebuffer(gBuffer);
     renderer.setWriteFramebuffer(nullptr);
     renderer.render(scene2,cam,"deferred");
